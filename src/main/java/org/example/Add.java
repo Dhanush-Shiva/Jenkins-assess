95bafc6f6580f3ff4f2d3e361e93a7b1401aae91
Dhanush <dhanushaurora369@gmail.com>
2026-09-23 11:01:48 +0530

    Added add

---
+++ src/main/java/org/example/Add.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 15;
+        int sum = num1 + num2;
+
+        System.out.println("======================================");
+        System.out.println("Executing Simple Addition in Jenkins");
+        System.out.println("First Number  : " + num1);
+        System.out.println("Second Number : " + num2);
+        System.out.println("Calculated Sum: " + sum);
+        System.out.println("======================================");
+    }
+}
